0d347da1c731e20ac2ac85c9619082b0f8fd1f9b
msalikhova <msalikhova@unomaha.edu>
2018-09-24 16:30:55 -0500

    changes in mysite/url file were done

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/',include('blog.urls',namespace='blog')),
+    path(' ',include('blog.urls')),
 ]
